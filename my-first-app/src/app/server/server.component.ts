c3f2e134804133dab5e83dce5dd2c46e60c50209
Ismael Quinto <ishmaelfifth@gmail.com>
2017-07-31 21:03:30 -0500

    parte2

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,8 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl:'./server.component.html'
 })
 export class ServerComponent{
 
+ serverID = 10;
+ serverStatus: string = 'offline';
+ getServerStatus(){
+     return this.serverStatus;
+ }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!' ;
+  serverName="Test server name";
+  userName : string;
+    constructor() {
+    console.log("AllowNewServer = " + this.allowNewServer);
 
-  constructor() { }
-
-  ngOnInit() {
+      setTimeout(() => {
+        this.allowNewServer = true;
+    console.log("AllowNewServer = " + this.allowNewServer);
+    },2000);
   }
+  ngOnInit(){
 
+  }
+  onCreateServer(){
+    this.serverCreationStatus = 'server was created!!' + this.serverName;
+  }
+  onUpdateServerName(event:any){
+    console.log(event);
+    this.serverName = event.target.value;
+  }
+  onResetUser(){
+    this.userName="";
+  }
 }
